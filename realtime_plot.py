f4e79fe4d347f4acb9f1f8e37e67b89034f2950a
Shion Honda <shionhonda@users.noreply.github.com>
2017-11-20 19:25:32 +0900

    bollinger band

--- realtime_plot.py
+++ realtime_plot.py
@@ -9,53 +9,61 @@ from IPython.display import display, clear_output
 
 api = pybitflyer.API()
 
-# 最終取引価格、移動平均、利率を格納する配列（確認用）
+# 最終取引価格、移動平均、分散、乖離率を格納する配列
 raws = []
-smoothed = [] 
-returns = []
-itr = 20 # 移動平均を取る幅
+sma1, sma2 = [], []
+sgm1, sgm2 = [], []
+dev1, dev2 = [], []
 
-current_price =  api.ticker(product_code = "BTC_JPY")['ltp']
-ltps = current_price*np.ones(itr) # 20サンプリング間の最終取引価格の配列
-smas = current_price*np.ones(2) # 1サンプリング前と現在の移動平均の配列
+# 移動平均を取る幅
+itr1 = 90 # 15 min
+itr2 = 360  # 60 mins
+
+current_price = api.ticker(product_code = "BTC_JPY")['ltp']
+ltps2 = current_price*np.ones(itr2) # 60分間の最終取引価格の配列
 
 plt.ion()
-fig = plt.figure(figsize=(16,8))
-# 2行1列に分けて1番目にプロット
+fig = plt.figure(figsize=(16,16))
 axe1 = fig.add_subplot(211)
-axe1.set_title("Raw price and Simple Moving Average")
-# 2行1列に分けて2番目にプロット
 axe2 = fig.add_subplot(212)
-axe2.set_title("Return per 10 seconds")
+axe1.set_title("Raw price and Bollinger band")
+axe2.set_title("Deviation rate")
 
 while True:
-    # 00秒に稼働
+    # 10秒ごとに稼働
     if datetime.now().strftime('%S') [1]== '0':
         clear_output(wait = True)
         tick = api.ticker(product_code = "BTC_JPY")
-        # 最終取引価格と移動平均の更新
-        ltps = np.hstack((ltps[1:itr], tick['ltp']))
-        smas = np.hstack((smas[1], ltps.mean()))
-        # 確認用データの更新
-        raws.append(ltps[itr-1])
-        smoothed.append(smas[1])
-        # 移動平均の1サンプリングあたりの利率
-        r = (smas[1]-smas[0])/smas[0]
-        returns.append(r)
-            
-        axe1.set_title("Raw price and Simple Moving Average")
-        axe2.set_title("Return per 10 seconds")
-        axe1.plot(raws, "grey", linewidth=1)
-        axe1.plot(smoothed, "g", linewidth=2)
-        axe2.plot(returns, "b", linewidth=2)
-        size = len(returns)
+        # 最終取引価格の更新
+        ltps2 = np.hstack((ltps2[1:itr2], tick['ltp']))
+        ltps1 = ltps2[itr2-itr1:itr2]
+        # プロット用データの更新
+        raws = np.append(raws, [ltps1[itr1-1]])
+        sma1 = np.append(sma1, [ltps1.mean()])
+        sma2.append(ltps2.mean())
+        sgm1 = np.append(sgm1, [ltps1.std()])
+        sgm2 = np.append(sgm2, [ltps2.std()])
+        size = len(raws)
+        dev1.append((raws[size-1]-sma1[size-1])/sma1[size-1])
+        dev2.append((raws[size-1]-sma2[size-1])/sma2[size-1])
+        # 1つめのプロット
+        axe1.plot(raws, "black", linewidth=2, label="Raw price")
+        axe1.plot(sma1, "r", linewidth=1, label="15min SMA")
+        axe1.plot(sma2, "g", linewidth=1, label="60min SMA")
+        axe1.plot(sma1+2*sgm1, "r", linewidth=1, linestyle="dashed", label="15min 2sigma")
+        axe1.plot(sma1-2*sgm1, "r", linewidth=1, linestyle="dashed")
+        axe1.plot(sma2+2*sgm2, "g", linewidth=1, linestyle="dashed", label="60min 2sigma")
+        axe1.plot(sma2-2*sgm2, "g", linewidth=1, linestyle="dashed")
+        axe1.legend(loc='upper left')
+        # 2つめのプロット
+        axe2.plot(dev1, "r", linewidth=2, label="15min Dev rate")
+        axe2.plot(dev2, "g", linewidth=2, label="60min Dev rate")
         axe2.plot(np.zeros(size), "black", linewidth=1)
-        axe2.plot(0.0001*np.ones(size), "grey", linewidth=1)
-        axe2.plot(-0.0001*np.ones(size), "grey", linewidth=1)
-        axe2.plot(0.0003*np.ones(size), "grey", linewidth=1)
-        axe2.plot(-0.0003*np.ones(size), "grey", linewidth=1)
-        plt.pause(1)
-        # 次の10秒まで休憩
+        axe2.legend(loc='upper left')
+        axe2.plot(0.005*np.ones(size), "grey", linewidth=1)
+        axe2.plot(-0.005*np.ones(size), "grey", linewidth=1)
+        display(fig)
+        # 次の00秒まで休憩
         time.sleep(7)
         axe1.cla()
         axe2.cla()
